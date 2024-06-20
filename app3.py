1025a28a13d0df4e04705d0fe156ccf142eaaf4f
priyanshgoantiya <priyanshgoantiya@gmail.com>
2024-06-20 16:24:39 +0530

    add lot of functionalityl

--- app3.py
+++ app3.py
@@ -7,6 +7,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.cluster import KMeans
 import seaborn as sns
 import numpy as np
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(layout='wide', page_title="Startup Analysis")
 
 # Load data
